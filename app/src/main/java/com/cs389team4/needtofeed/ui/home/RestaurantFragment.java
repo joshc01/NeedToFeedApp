9eb85b5ae6eb11c491f01ad9fc43abb6b0973c8f
Josh <josh@Joshuas-MacBook-Air.local>
2021-11-07 15:44:23 -0500

    update to click on restaurants to bring you to their items

--- app/src/main/java/com/cs389team4/needtofeed/ui/home/RestaurantFragment.java
+++ app/src/main/java/com/cs389team4/needtofeed/ui/home/RestaurantFragment.java
@@ -1,60 +1,28 @@
 package com.cs389team4.needtofeed.ui.home;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-import com.amplifyframework.datastore.generated.model.Restaurant;
 import com.cs389team4.needtofeed.databinding.FragmentRestaurantBinding;
-import com.cs389team4.needtofeed.models.RestaurantViewModel;
-import com.cs389team4.needtofeed.utils.ListFragment;
-import com.cs389team4.needtofeed.utils.Utils;
-import com.cs389team4.needtofeed.utils.ViewModel;
 
-import java.util.UUID;
+public class RestaurantFragment extends Fragment {
+    private FragmentRestaurantBinding binding = null;
 
-public class RestaurantFragment extends ListFragment<Restaurant> {
-    @NonNull
+    @Nullable
     @Override
-    public Restaurant createModel() {
-        return Restaurant.builder()
-                .name("a name")
-                .category("a category")
-                .location("a location")
-                .timeOpen(null)
-                .timeClose(null)
-                .image(null)
-                .build();
-    }
-
-    @NonNull
-    @Override
-    public Restaurant updateModel(Restaurant model) {
-        return model.copyOfBuilder()
-                .name(UUID.randomUUID().toString())
-                .build();
-    }
-
-    @NonNull
-    @Override
-    public Class<? extends Restaurant> getModelClass() {
-        return Restaurant.class;
-    }
-
-    @NonNull
-    @Override
-    public ViewModel<Restaurant> getViewModel(@NonNull Restaurant model) {
-        return new RestaurantViewModel(model);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentRestaurantBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
-    public void onClick(Restaurant item) {
-        Utils util = new Utils();
-        util.showMessage(getContext(), "Item clicked");
-    }
-
-    @Override
-    public boolean onLongClick(Restaurant item) {
-        return false;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
