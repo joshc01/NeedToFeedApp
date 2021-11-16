ed938008594395792189a66cb830b841a410caa7
Josh <josh@Joshuas-MacBook-Air.local>
2021-11-16 16:52:24 -0500

    visually added delivery and pickup buttons to home tab

--- app/src/main/java/com/cs389team4/needtofeed/ui/home/RestaurantFragment.java
+++ app/src/main/java/com/cs389team4/needtofeed/ui/home/RestaurantFragment.java
@@ -1,12 +1,16 @@
 package com.cs389team4.needtofeed.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import com.cs389team4.needtofeed.databinding.FragmentRestaurantBinding;
@@ -24,5 +28,28 @@ public class RestaurantFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        AppCompatButton btnDelivery = binding.deliveryButton;
+        AppCompatButton btnPickup = binding.pickupButton;
+
+
+        btnDelivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnDelivery.setBackgroundColor(Color.DKGRAY);
+                btnPickup.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+        btnPickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnPickup.setBackgroundColor(Color.DKGRAY);
+                btnDelivery.setBackgroundColor(Color.WHITE);
+            }
+        });
+
+
     }
+
 }
