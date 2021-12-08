7778656777d77e16709639c347d46e4e4f273574
Josh <josh@Joshuas-MacBook-Air.local>
2021-12-08 16:26:51 -0500

    added settings fragment layout

--- app/src/main/java/com/cs389team4/needtofeed/ui/SettingsFragment.java
+++ app/src/main/java/com/cs389team4/needtofeed/ui/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.cs389team4.needtofeed.ui;
 
 import android.os.Bundle;
 
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.cs389team4.needtofeed.R;
