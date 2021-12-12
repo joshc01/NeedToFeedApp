ed898f1f7fdd7301768591cbd6a6fdff8273d312
Josh <josh@Joshuas-MacBook-Air.local>
2021-12-12 15:46:52 -0500

    added complete layout for profile tab

--- app/src/main/java/com/cs389team4/needtofeed/ui/SettingsFragment.java
+++ app/src/main/java/com/cs389team4/needtofeed/ui/SettingsFragment.java
@@ -1,16 +1,32 @@
 package com.cs389team4.needtofeed.ui;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.cs389team4.needtofeed.R;
+import com.cs389team4.needtofeed.databinding.FragmentAccountBinding;
+import com.cs389team4.needtofeed.databinding.FragmentSettingsBinding;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends Fragment {
+    private FragmentSettingsBinding binding = null;
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
