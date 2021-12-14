8aa7056377b82b5ad2b2956536f84d6fb18aa8bc
Josh <josh@Joshuas-MacBook-Air.local>
2021-12-14 16:57:06 -0500

    updates to account edit fragments

--- app/src/main/java/com/cs389team4/needtofeed/ui/profile/EditPasswordFragment.java
+++ app/src/main/java/com/cs389team4/needtofeed/ui/profile/EditPasswordFragment.java
@@ -12,15 +12,8 @@ import android.view.ViewGroup;
 
 import com.cs389team4.needtofeed.R;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.appcompat.widget.AppCompatTextView;
 import androidx.navigation.Navigation;
 
-import com.amplifyframework.auth.AuthUserAttribute;
-import com.amplifyframework.auth.AuthUserAttributeKey;
 import com.amplifyframework.core.Amplify;
 import com.cs389team4.needtofeed.MainActivity;
 import com.cs389team4.needtofeed.R;
@@ -40,27 +33,18 @@ public class EditPasswordFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-/*
-        //Grabs the user name from AWS attributes
-        AuthUserAttribute password = MainActivity.userAttrs.get(2);
-        String name_string = name.getValue();
-        TextView name_tv = binding.editPasswordLbl;
-        name_tv.setText(name_string);
+
+        String currentPassword = binding.currentPasswordContainer.toString();
+        String newPassword = binding.newPasswordContainer.toString();
 
         //Saves and updates user attribute name on click
         binding.saveLblContainer.setOnClickListener(v -> {
-                    AuthUserAttribute newName = new AuthUserAttribute(AuthUserAttributeKey.(),
-                            binding.editPasswordLbl.getText().toString());
-                    MainActivity.userAttrs.set(2, newName);
-                    Amplify.Auth.updateUserAttribute(newName,
-                            result -> Log.i("AuthDemo", "Updated user attribute = "
-                                    + result.toString()),
-                            error -> Log.e("AuthDemo", "Failed to update user attribute.", error)
-                    );
+            Amplify.Auth.updatePassword(currentPassword, newPassword,
+                    () -> Log.i("AuthQuickstart", "Updated password successfully"),
+                    error -> Log.e("AuthQuickstart", error.toString()));
 
-                    Navigation.findNavController(view).navigate(R.id.navigate_profile);
-                }
+            Navigation.findNavController(view).navigate(R.id.navigate_profile);
+        }
         );
-  */
     }
 }
